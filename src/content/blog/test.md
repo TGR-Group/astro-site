8c22e6b77eccc23bfae3a28cd0162dd56315788b
Laddge <dev.laddge@gmail.com>
2023-04-20 04:12:25 +0000

    setup contents collection

---
+++ src/content/blog/test.md
@@ -0,0 +1,12 @@
+---
+title: Title
+author: laddge
+tags: [example]
+colorHue: 0
+pubDate: 2023-04-19
+---
+
+# Test
+
+- hoge
+- fuga
---
+++ src/content/config.ts
@@ -0,0 +1,28 @@
+import { z, defineCollection } from "astro:content";
+import type { ZodLiteral, Primitive } from 'zod'
+import { Octokit } from '@octokit/rest'
+
+const octokit = new Octokit({
+  auth: import.meta.env.GH_TOKEN
+})
+
+const members = await octokit.paginate('GET /orgs/{org}/members', { org: 'TGR-Group' })
+const membersUnion = z.union(
+  members.map(member => z.literal(member.login)) as unknown as [
+    ZodLiteral<Primitive>,
+    ZodLiteral<Primitive>,
+    ...ZodLiteral<Primitive>[]
+  ]
+)
+
+const blog = defineCollection({
+  schema: z.object({
+    title: z.string().nonempty(),
+    author: membersUnion,
+    tags: z.string().array().min(1).max(5),
+    colorHue: z.number().min(0).max(360),
+    pubDate: z.date()
+  })
+})
+
+export const collections = { blog }
