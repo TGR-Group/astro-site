b4e609f40c060f92238696648395a8eb207f92db
Laddge <dev.laddge@gmail.com>
2023-10-20 13:24:14 +0000

    upgrade astro

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,4 +1,4 @@
 /// <reference path="../.astro/types.d.ts" />
-/// <reference types="astro/client-image" />
+/// <reference types="astro/client" />
 /// <reference types="astro-imagetools" />
 declare module 'astro-imagetools/components'
